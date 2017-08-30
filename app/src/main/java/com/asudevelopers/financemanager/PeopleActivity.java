e16fff97beb2a4b9cbe7be0a6968333bf201776f
KB429861 <kb429861@gmail.com>
2017-08-30 12:17:21 +0700

    Toolbar!

--- app/src/main/java/com/asudevelopers/financemanager/PeopleActivity.java
+++ app/src/main/java/com/asudevelopers/financemanager/PeopleActivity.java
@@ -1,7 +1,9 @@
 package com.asudevelopers.financemanager;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class PeopleActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class PeopleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_people);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("tet");
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
