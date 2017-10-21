40b42499413e178d2493c2621274fe9c9d47b240
KB429861 <kb429861@gmail.com>
2017-10-21 15:16:14 +0700

    Person Contact Picker.

--- app/src/main/java/com/asudevelopers/financemanager/mvp/view/PersonView.java
+++ app/src/main/java/com/asudevelopers/financemanager/mvp/view/PersonView.java
@@ -6,4 +6,6 @@ import com.asudevelopers.financemanager.mvp.model.entity.Person;
 public interface PersonView extends BaseView {
 
     void showPerson(Person person);
+
+    void showPersonInfo(String name, String phone);
 }
--- app/src/main/java/com/asudevelopers/financemanager/ui/activity/PersonActivity.java
+++ app/src/main/java/com/asudevelopers/financemanager/ui/activity/PersonActivity.java
@@ -1,6 +1,10 @@
 package com.asudevelopers.financemanager.ui.activity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -20,6 +24,8 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class PersonActivity extends BaseActivity implements PersonView {
+
+    private static final int RESULT_PICK_CONTACT = 65535;
 
     @BindView(R.id.et_name)
     EditText nameEditText;
@@ -65,6 +71,47 @@ public class PersonActivity extends BaseActivity implements PersonView {
         onBackPressed();
     }
 
+    @OnClick(R.id.btn_search)
+    public void onSearchClick(View view) {
+        Intent contactPickerIntent = new Intent(
+                Intent.ACTION_PICK,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+        );
+        startActivityForResult(contactPickerIntent, RESULT_PICK_CONTACT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    contactPicked(data);
+                    break;
+            }
+        }
+    }
+
+    private void contactPicked(Intent data) {
+        try {
+            Uri uri = data.getData();
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            if (cursor != null) {
+                cursor.moveToFirst();
+                int phoneIndex = cursor.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER);
+                int nameIndex = cursor.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+                String name = cursor.getString(nameIndex);
+                String phone = cursor.getString(phoneIndex);
+                showPersonInfo(name, phone);
+                cursor.close();
+            }
+        } catch (Exception e) {
+            showError(e);
+        }
+    }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
@@ -73,7 +120,12 @@ public class PersonActivity extends BaseActivity implements PersonView {
 
     @Override
     public void showPerson(Person person) {
-        nameEditText.setText(person.getName());
-        phoneEditText.setText(person.getPhone());
+        showPersonInfo(person.getName(), person.getPhone());
+    }
+
+    @Override
+    public void showPersonInfo(String name, String phone) {
+        nameEditText.setText(name);
+        phoneEditText.setText(phone);
     }
 }
